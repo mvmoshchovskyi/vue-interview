98669cfc168a18c88c8fac773c9e123391120582
Mykhailo Moshchovskyi <mykhailo.m@vivint.com>
2024-12-24 11:22:53 +0200

    adds primevue styles themes and icons

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { ComputedRef, ref } from 'vue'
+
+interface IMenuItem {
+  label: string
+  icon: string
+  path: string
+  show: ComputedRef<boolean>
+}
+
+const items = ref<IMenuItem[]>([
+  {
+    label: 'Authorizatiion',
+    icon: 'pi pi-user',
+    path: '/auth',
+    // show: computed((): boolean => !userStore.userId)
+  },
+  {
+    label: 'Add',
+    icon: 'pi pi-plus',
+    path: '/',
+    // show: computed((): boolean => !!userStore.userId)
+  },
+  {
+    label: 'Interviews List',
+    icon: 'pi pi-list',
+    path: '/list',
+    // show: computed((): boolean => !!userStore.userId)
+  },
+  {
+    label: 'Statistic',
+    icon: 'pi pi-chart-pie',
+    path: '/statistic',
+    // show: computed((): boolean => !!userStore.userId)
+  },
+])
+</script>
+
+<template>
+  <app-menubar :model="items" class="menu">
+    <template #item="{ item, props }">
+      <router-link :to="item.path" class="flex align-items-center" v-bind="props.action">
+        <span :class="item.icon" class="p-menuitem-icon"></span>
+        <span class="ml-2">{{ item.label }}</span>
+      </router-link>
+    </template>
+  </app-menubar>
+</template>
+
+<style scoped>
+.menu {
+  margin: 30px 0;
+}
+
+.menu-exit {
+  cursor: pointer;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import './assets/main.css'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+import Aura from '@primevue/themes/aura'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import PrimeVue from 'primevue/config'
+import Menubar from 'primevue/menubar'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +16,11 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+});
+app.component('app-menubar', Menubar)
 
 app.mount('#app')
